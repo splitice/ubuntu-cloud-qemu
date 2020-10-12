ee3693b70d62a26fcc5f7a074eea9e22384d1c47
Mathew Heard <splitice@users.noreply.github.com>
2020-10-12 23:00:02 +1100

    ...

--- prepare.sh
+++ prepare.sh
@@ -3,13 +3,10 @@ chmod +x /scripts/*
 apt-get install -y cloud-image-utils qemu qemu-system net-tools iproute2 ifupdown udhcpd jq openssh-client iputils-ping ansible sshpass tcpdump nano
 
 # This is already in qcow2 format.
-img=ubuntu-18.04-server-cloudimg-amd64.img
-if [ ! -f "$img" ]; then
-  wget "https://cloud-images.ubuntu.com/releases/18.04/release/${img}"
+if [ ! -f "/image" ]; then
+  wget "https://cloud.debian.org/images/cloud/buster/20200928-407/debian-10-generic-amd64-20200928-407.qcow2" -O /image
 
   # sparse resize: does not use any extra space, just allows the resize to happen later on.
   # https://superuser.com/questions/1022019/how-to-increase-size-of-an-ubuntu-cloud-image
-  qemu-img resize "$img" +12G
+  qemu-img resize "/image" +12G
 fi
-
-mv ubuntu-18.04-server-cloudimg-amd64.img /image
