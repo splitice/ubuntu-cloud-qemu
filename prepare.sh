6693ee8c323eb7c28a06cb95a921221fa2fb926e
Mathew Heard <splitice@users.noreply.github.com>
2020-10-13 00:57:51 +1100

    ...

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 chmod +x /scripts/*
 
-apt-get install -y cloud-image-utils qemu qemu-system net-tools iproute2 ifupdown udhcpd jq openssh-client iputils-ping ansible sshpass tcpdump nano
+apt-get install -y cloud-image-utils qemu qemu-system net-tools iproute2 ifupdown udhcpd jq openssh-client iputils-ping ansible sshpass tcpdump nano procps
 
 # This is already in qcow2 format.
 if [ ! -f "/image" ]; then
